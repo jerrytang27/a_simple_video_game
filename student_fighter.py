c2d904aa923736f701ae8f3913256d9824977316
Jerry Tang <JerryTang@Jerrys-MacBook-Pro.local>
2018-01-08 15:30:59 +0800

    first commit

---
+++ student_fighter.py
@@ -0,0 +1,16 @@
+class StudentFighter():
+
+    def __init__(self, strength, health, name):
+        self.strength = strength
+        self.health = health
+        self.name = name
+    
+    def attack(self, opponent):
+        opponent.health -= self.strength
+        message = "{} has {} health points remaining".format(opponent.name, opponent.health)
+        print(message)
+
+kalu = StudentFighter(strength=3, health=100, name="Kalu")
+david = StudentFighter(strength=5, health=100, name="David")
+
+kalu.attack(david)
